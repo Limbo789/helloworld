309c4f972e2d13936bfc52c6569472ca3124d33b
Limbo789 <41985161+Limbo789@users.noreply.github.com>
2020-05-21 10:36:45 +0800

    master says

---
+++ src/com/xulinbo/Master.java
@@ -0,0 +1,16 @@
+package com.xulinbo;
+
+/**
+ * 2020/05/21/021
+ * 10:35
+ * craeted by limboo
+ **/
+public class Master {
+
+    public static void main(String[] args) {
+
+    }
+    public static void Says(){
+        System.out.println("this is master saying;");
+    }
+}
