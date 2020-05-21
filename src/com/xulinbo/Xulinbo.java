9d4514f346164177b39976e52a28795884a6028b
Limbo789 <41985161+Limbo789@users.noreply.github.com>
2020-05-21 10:52:29 +0800

    xulinbo says

---
+++ src/com/xulinbo/Xulinbo.java
@@ -0,0 +1,16 @@
+package com.xulinbo;
+
+/**
+ * 2020/05/21/021
+ * 10:45
+ * craeted by limboo
+ **/
+public class Xulinbo {
+
+    public static void main(String[] args) {
+
+    }
+    public static void says(){
+        System.out.println("this is xulinbo saying");
+    }
+}
